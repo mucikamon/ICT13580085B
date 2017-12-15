b3da5dae834b595d51b98a078f5fa96d44c98861
Supawit <mucikamon@hotmail.com>
2017-12-15 22:27:17 +0700

    rai gor dai

---
+++ ICT13580085B/MyPage2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ICT13580085B
+{
+    public class MyPage2 : ContentPage
+    {
+        public MyPage2()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
---
+++ ICT13580085B/StackPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ICT13580085B
+{
+    public partial class Stackpage : ContentPage
+    {
+        public Stackpage()
+        {
+            InitializeComponent();
+        }
+    }
+}
